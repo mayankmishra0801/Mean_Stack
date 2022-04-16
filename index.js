d051eb2da85693303e16c1f462bcc319f0186eb8
mayankmishra0801 <mayank08mrinal04@gmail.com>
2022-04-16 12:29:36 +0530

    Routing Code has been added

--- index.js
+++ index.js
@@ -31,27 +31,28 @@ app.listen(200,(req,res) => {  //tells the express application to listen to port
 
 
 
-const express = require('express');
-const app = express();
+//const express = require('express');
+//const app = express();
 
-const path = require('path');//default library for path specification
-const router = express.Router();// router library being called
+//const path = require('path');//default library for path specification
+//const router = express.Router();// router library being called
 
-app.get('/',(req,res) => {//root
-    res.sendFile(path.join(__dirname+'/views/home.html'));//render
-})
+//app.get('/',(req,res) => {//root
+  //  res.sendFile(path.join(__dirname+'/views/home.html'));//render
 
-app.get('/about',(req,res) => {
-    res.sendFile(path.join(__dirname+'/views/about.html')); //render
+//})
+
+//app.get('/about',(req,res) => {
+  //  res.sendFile(path.join(__dirname+'/views/about.html')); //render
     
-})
+//})
 
-app.get('/contact',(req,res) => {
+//app.get('/contact',(req,res) => {
 
-    res.sendFile(path.join(__dirname+'/views/contact.html')); //render
+  //  res.sendFile(path.join(__dirname+'/views/contact.html')); //render
     
     
-})
+//})
 
 
 
@@ -61,6 +62,58 @@ app.get('/contact',(req,res) => {
 
 
 
-app.listen(200,(req,res)=>{
-    console.log('Server Started');
-})
+//app.listen(200,(req,res)=>{
+  //  console.log('Server Started');
+//})
+
+var http = require('http'); 
+
+  
+// Create a server object 
+
+http.createServer(function (req, res) { 
+
+      
+
+    // http header 
+
+    res.writeHead(200, {'Content-Type': 'text/html'});  
+
+      
+
+    var url = req.url; 
+
+      
+
+    if(url ==='/about') { 
+
+        res.write(' Welcome to about us page');  
+
+        res.end();  
+
+    } 
+
+    else if(url ==='/contact') { 
+
+        res.write(' Welcome to contact us page');  
+
+        res.end();  
+
+    } 
+
+    else { 
+
+        res.write('Welcome To  Mean Stack!');  
+
+        res.end();  
+
+    } 
+
+}).listen(3000, function() { 
+
+      
+
+    // The server object listens on port 3000 
+
+    console.log("server start at port 3000"); 
+}); 
